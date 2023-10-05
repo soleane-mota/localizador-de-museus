f81e91624558341326edc06651d766721bc123b8
Soleane Mota <sem.sol@gmail.com>
2023-10-05 18:03:23 -0400

    feat: create MuseumService class

---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,44 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * MuseumService class.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+
+  private final MuseumFakeDatabase museumFakeDatabase;
+
+  @Autowired
+  public MuseumService(MuseumFakeDatabase museumFakeDatabase) {
+    this.museumFakeDatabase = museumFakeDatabase;
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    CoordinateUtil coordinateUtil = new CoordinateUtil();
+
+    if (coordinateUtil.isCoordinateValid(museum.getCoordinate())) {
+      return museumFakeDatabase.saveMuseum(museum);
+    }
+
+    throw new InvalidCoordinateException();
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
